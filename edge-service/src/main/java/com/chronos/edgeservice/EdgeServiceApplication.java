025c5876805784e5722f1c6324bd81e4a627a60a
Nerea García <nereitagp12@gmail.com>
2021-03-07 18:31:26 +0100

    added Docker/postgresql configuration

--- edge-service/src/main/java/com/chronos/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/chronos/edgeservice/EdgeServiceApplication.java
@@ -2,8 +2,10 @@ package com.chronos.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
